6ad61bbeeffd00792f0d14864f8ad3f387991844
Yoqub <yyboboraximov@gmail.com>
2022-07-29 12:13:22 +0500

    task5

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,23 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+        boolean is_leap_year = false;
+
+        if (year % 4 == 0) {
+            is_leap_year = true;
+            if (year % 100 == 0) {
+                if (year % 400 == 0)
+                    is_leap_year = true;
+                else
+                    is_leap_year = false;
+            }
+        }
+        else
+            is_leap_year = false;
+        if (!is_leap_year)
+            System.out.println("leap");
+        else
+            System.out.println("not leap");
     }
-}
+    }
+
--- src/main/java/school/mjc/stage0/conditions/task5/SwitchDaysInMonthPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/SwitchDaysInMonthPrinter.java
@@ -2,5 +2,6 @@ package school.mjc.stage0.conditions.task5;
 
 public class SwitchDaysInMonthPrinter {
     public void amountOfDays(int month) {
+
     }
 }
